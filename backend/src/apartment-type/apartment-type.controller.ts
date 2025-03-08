50763325b158eb60025a2e6edacf925dd16f3109
Mohamed Khaled <tmtmgames2021@gmail.com>
2025-03-08 17:09:29 +0200

    add ApartmentTypes auth

--- backend/src/apartment-type/apartment-type.controller.ts
+++ backend/src/apartment-type/apartment-type.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ApartmentTypeService } from './apartment-type.service';
 import { CreateApartmentTypeDto } from './dto/create-apartment-type.dto';
 import { UpdateApartmentTypeDto } from './dto/update-apartment-type.dto';
 import { ApiCreateApartmentType, ApiDeleteApartmentTypeById,ApiGetAllApartmentTypes,ApiGetApartmentTypeById,ApiUpdateApartmentTypeById} from './swaggerHelper'
+import { JwtAuthGuard } from 'src/common/guards';
 @ApiTags('Apartment Types')
 @Controller('apartment-types')
 export class ApartmentTypeController {
@@ -11,6 +12,7 @@ export class ApartmentTypeController {
 
   @Post()
   @ApiCreateApartmentType()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createApartmentTypeDto: CreateApartmentTypeDto) {
     return this.apartmentTypeService.create(createApartmentTypeDto);
   }
@@ -28,12 +30,14 @@ export class ApartmentTypeController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   @ApiUpdateApartmentTypeById()
   update(@Param('id') id: string, @Body() updateApartmentTypeDto: UpdateApartmentTypeDto) {
     return this.apartmentTypeService.update(+id, updateApartmentTypeDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   @ApiDeleteApartmentTypeById()
   remove(@Param('id') id: string) {
     return this.apartmentTypeService.remove(+id);
